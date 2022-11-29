4c1daf47d7932729582a99d1ae4998189d894e88
dladncks1217 <dlaxodud1217@gmail.com>
2022-11-29 21:27:31 +0900

    feat(constants): 상수 문자열 추가

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,11 +1,12 @@
 const MESSAGE = {
   GAME_START: '숫자 야구 게임을 시작합니다.',
-  NUMBER_INPUT: (input) => `숫자를 입력해주세요 : ${input}`,
+  NUMBER_INPUT: `숫자를 입력해주세요 : `,
   STRIKE_COUNT: (count) => `${count}스트라이크`,
   BALL_COUNT: (count) => `${count}볼`,
+  NOTHING: '낫싱',
   GAME_END: '3개의 숫자를 모두 맞히셨습니다! 게임 종료',
   GAME_RETRY: '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.',
-  INPUT_ERROR: '[ERROR] 잘못된 값을 입력하셨습니다. 다시 입력해주세요!',
+  INPUT_ERROR: '[ERROR] 잘못된 값을 입력하셨습니다. 게임이 종료됩니다.',
 };
 
 module.exports = MESSAGE;
