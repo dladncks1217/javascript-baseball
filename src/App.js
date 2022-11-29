1cc476c4f16529700f35caf8ded4cda5f7dad759
dladncks1217 <dlaxodud1217@gmail.com>
2022-11-29 21:28:13 +0900

    feat(App): 각 기능들 merge

--- src/App.js
+++ src/App.js
@@ -1,5 +1,55 @@
+const { Console } = require('@woowacourse/mission-utils');
+const GameMachine = require('./domain/GameMachine');
+const InputView = require('./UI/InputView');
+const OutputView = require('./UI/OutputView');
+const getBallCount = require('./utils/getBallCount');
+const getStrikeCount = require('./utils/getStrikeCount');
+const verifyNumber = require('./utils/verifyNumber');
+
 class App {
-  play() {}
+  #myGame;
+
+  play() {
+    OutputView.gameStart();
+    this.#myGame = new GameMachine();
+    this.inputMyNumber();
+  }
+
+  inputMyNumber() {
+    InputView.inputMyAnswer((userInput) => {
+      try {
+        if (verifyNumber(userInput)) {
+          return this.getResult(userInput);
+        }
+      } catch (err) {
+        return this.errorQuitGame();
+      }
+    });
+  }
+
+  getResult(myInput) {
+    const { strike, ball } = this.#myGame.getResult(
+      myInput,
+      getStrikeCount,
+      getBallCount
+    );
+    OutputView.printballCount(strike, ball);
+    if (this.#myGame.isFinishedGame(myInput)) return this.quitGame();
+    return this.inputMyNumber();
+  }
+
+  quitGame() {
+    OutputView.endGame();
+    return Console.close();
+  }
+
+  errorQuitGame() {
+    OutputView.printError();
+    return Console.close();
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
